135f584b1070c5476cdd229ba858e9552bf42398
faustiB <fbg171@gmail.com>
2021-10-31 10:29:57 +0100

    Import of necessary libraries

--- fase1.c
+++ fase1.c
@@ -1,5 +1,8 @@
 #include <linux/init.h>
 #include <linux/module.h>
+#include <linux/kernel.h>
+#include <linux/gpio.h>
+#include <linux/interrupt.h>
 #include <linux/fs.h>
 #include <linux/uaccess.h>
 
