a83e61c06f9adad52d6786bad41fe420014b3fdf
faustiB <fbg171@gmail.com>
2021-11-04 18:19:09 +0100

    First config of buttons and led

--- fase1.c
+++ fase1.c
@@ -10,13 +10,13 @@
 MODULE_LICENSE("FBG");
 
 
-static unsigned int gpioLED1=23; //Hardcode del pin del led1
-static unsigned int gpioLED2=24; //Hardcode del pin del led3
+static unsigned int gpioLED1=20; //Hardcode del pin del led1
+static unsigned int gpioLED2=16; //Hardcode del pin del led3
 
 static bool led1On=0; //Bool para determinar si estaencendido o no. Se usa como apagado por defecto. 
 static bool led2On=0; //Bool para determinar si estaencendido o no. Se usa como apagado por defecto. 
 
-static unsigned int gpioBUT1=22; //Hardcode del pin del boton1
+static unsigned int gpioBUT1=26; //Hardcode del pin del boton1
 static unsigned int gpioBUT2=27; //Hardcode del pin del boton2
 static unsigned int gpioBUT3=5; //Hardcode del pin del boton3
 static unsigned int gpioBUT4=6; //Hardcode del pin del boton4
@@ -24,13 +24,25 @@ static unsigned int gpioBUT4=6; //Hardcode del pin del boton4
 static int __init my_init(void) {
 	//Inicializacion + check de los pines de los leds. 	
 	printk(KERN_INFO "FASE 1 : Initializing Fase 1 LKM \n");
-	if(!gpio_is_valid(gpioLED1) || !gpio_is_valid(gpioLED2) ||
- 	   !gpio_is_valid(gpioBUT1) || !gpio_is_valid(gpioBUT2) ||
- 	   !gpio_is_valid(gpioBUT3) || !gpio_is_valid(gpioBUT4) ){
+	if(!gpio_is_valid(gpioLED1) || !gpio_is_valid(gpioLED2)){
 		printk(KERN_INFO "FASE 1 :One of the LEDs Hardcoded it is not correct. Please check.  \n");
 	}
 
 
+	ledOn1 = true;
+   	gpio_request(gpioLED1, "sysfs");          
+   	gpio_direction_output(gpioLED1, ledOn1);   
+   	gpio_export(gpioLED1, false);             
+   	gpio_request(gpioBUT1, "sysfs");       
+   	gpio_direction_input(gpioBUT1);        
+   	gpio_set_debounce(gpioBUT1, 200);      
+   	gpio_export(gpioBUT1, false);         
+   	printk(KERN_INFO "GPIO_TEST: The button state is currently: %d\n", gpio_get_value(gpioBUT1));
+ 	
+	
+	
+	
+	
 	return 0;
 }
 
